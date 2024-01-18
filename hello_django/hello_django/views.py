51df0fd4c3ba75c2c46a084d156597af6811fc78
VishnyakAlena <vishnyak-elena@mail.ru>
2024-01-19 00:25:35 +0300

    reversed_text

--- hello_django/hello_django/views.py
+++ hello_django/hello_django/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render
 def about(request):
     a = 5 + 6
     return render(request, 'about.html', {'gretting':a})
+def reverse(request):
+    user_text=request.GET['username']
+    reverse=user_text[::-1]
+    return render(request, 'reverse.html', {'word':reverse})
 def home(request):
     return HttpResponse('This is my home')
